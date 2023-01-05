b04ff16e629526cdf97b42896aa611389913b7b6
kopiHyper <oktawiankopica2001@gmail.com>
2023-01-05 21:24:38 +0100

    Configured amonut input and onSubmitted

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -7,6 +7,20 @@ class NewTransaction extends StatelessWidget {
 
   NewTransaction(this.addTx);
 
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTx(
+      enteredTitle,
+      enteredAmount,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -18,20 +32,24 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
+              onSubmitted: (_) => {
+                submitData,
+              },
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.numberWithOptions(
+                decimal: true,
+              ),
+              onSubmitted: (_) => {
+                submitData,
+              },
             ),
             TextButton(
               child: Text('Add transaction'),
               style: TextButton.styleFrom(foregroundColor: Colors.purple),
-              onPressed: () => {
-                addTx(
-                  titleController.text,
-                  double.parse(amountController.text),
-                ),
-              },
+              onPressed: submitData,
             ),
           ],
         ),
