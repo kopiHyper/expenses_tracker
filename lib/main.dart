e0309ca0b46a335b6e3d855c01058d4e490b5210
kopiHyper <oktawiankopica2001@gmail.com>
2023-01-05 20:27:04 +0100

    Added Scroll View and displaying inputed data

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:expenses_tracker/transaction.dart';
+import './widgets/user_transaction.dart';
 import 'package:flutter/material.dart';
 import 'dart:core';
-import 'package:intl/intl.dart';
 
 void main() => runApp(MyWidget());
 
@@ -18,20 +17,8 @@ class MyWidget extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'New Shirt',
-      amount: 19.99,
-      date: DateTime.now(),
-    )
-  ];
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -39,81 +26,16 @@ class MyHomePage extends StatelessWidget {
       // appBar: AppBar(
       //   title: Text('Flutter App'),
       // ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Card(
-              child: Text('CHART!'),
-            ),
-            Card(
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Title'),
-                    ),
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Amount'),
-                    ),
-                    TextButton(
-                      child: Text('Add transaction'),
-                      style:
-                          TextButton.styleFrom(foregroundColor: Colors.purple),
-                      onPressed: () => {},
-                    ),
-                  ],
-                ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Card(
+                child: Text('CHART!'),
               ),
-            ),
-            Column(
-              children: transactions.map((tx) {
-                return Card(
-                    child: Row(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.purple, width: 2),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        '\$${tx.amount}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          tx.title,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
-                        ),
-                        Text(
-                          DateFormat('EEE, MMMM dd, y').format(tx.date),
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey.shade700,
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ));
-              }).toList(),
-            )
-          ],
+              UserTransaction(),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final Function addTx;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  NewTransaction(this.addTx);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleController,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: amountController,
+            ),
+            TextButton(
+              child: Text('Add transaction'),
+              style: TextButton.styleFrom(foregroundColor: Colors.purple),
+              onPressed: () => {
+                addTx(
+                  titleController.text,
+                  double.parse(amountController.text),
+                ),
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
