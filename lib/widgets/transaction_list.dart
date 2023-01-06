a626e30373c126c78a7e4d6161d72b5b85b4693a
kopiHyper <oktawiankopica2001@gmail.com>
2023-01-06 16:02:52 +0100

    Currency changed to PLN

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -26,7 +26,7 @@ class TransactionList extends StatelessWidget {
                   ),
                   padding: EdgeInsets.all(10),
                   child: Text(
-                    '\$${transactions[index].amount.toStringAsFixed(2)}',
+                    '${transactions[index].amount.toStringAsFixed(2)} PLN',
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 16,
