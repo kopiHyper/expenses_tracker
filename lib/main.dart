dd8791567503913e8c529c4671822c6e8496d7cf
kopiHyper <oktawiankopica2001@gmail.com>
2023-01-25 18:56:37 +0100

    Splitting widgets for more readable and cleaner code

--- lib/main.dart
+++ lib/main.dart
@@ -83,58 +83,59 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
-    final isLandscape = mediaQuery.orientation == Orientation.landscape;
-    final txListWidget = Container(
-      height: mediaQuery.size.height * 0.5,
-      child: TransactionList(_userTransactions, _deleteTransaction),
-    );
-    final appBody = SafeArea(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+  List<Widget> _buildLandscapeContent(
+    MediaQueryData mediaQuery,
+    Widget txListWidget,
+  ) {
+    return [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          if (isLandscape)
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Show chart',
-                  style: TextStyle(
-                      fontSize: 16,
-                      decoration: TextDecoration.none,
-                      fontFamily: 'Quicksand',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black),
-                ),
-                Switch.adaptive(
-                  activeColor: Colors.purple,
-                  value: _showChart,
-                  onChanged: (val) {
-                    setState(() {
-                      _showChart = val;
-                    });
-                  },
-                )
-              ],
-            ),
-          if (!isLandscape)
-            Container(
-                height: mediaQuery.size.height * 0.2,
-                child: Chart(_recentTransactions)),
-          if (!isLandscape) txListWidget,
-          if (isLandscape)
-            _showChart
-                ? Container(
-                    height: mediaQuery.size.height * 0.6,
-                    child: Chart(_recentTransactions),
-                  )
-                : txListWidget,
+          const Text(
+            'Show chart',
+            style: TextStyle(
+                fontSize: 16,
+                decoration: TextDecoration.none,
+                fontFamily: 'Quicksand',
+                fontWeight: FontWeight.bold,
+                color: Colors.black),
+          ),
+          Switch.adaptive(
+            activeColor: Colors.purple,
+            value: _showChart,
+            onChanged: (val) {
+              setState(() {
+                _showChart = val;
+              });
+            },
+          )
         ],
       ),
-    );
+      _showChart
+          ? Container(
+              height: mediaQuery.size.height * 0.6,
+              child: Chart(_recentTransactions),
+            )
+          : txListWidget,
+    ];
+  }
 
+  List<Widget> _buildPortraitContent(
+    MediaQueryData mediaQuery,
+    Widget txListWidget,
+  ) {
+    return [
+      Container(
+        height: mediaQuery.size.height * 0.2,
+        child: Chart(_recentTransactions),
+      ),
+      txListWidget,
+    ];
+  }
+
+  Widget _buildPage(
+    Widget appBody,
+  ) {
     return Platform.isIOS
         ? CupertinoPageScaffold(
             navigationBar: CupertinoNavigationBar(
@@ -165,5 +166,34 @@ class _MyHomePageState extends State<MyHomePage> {
               onPressed: () => _showAddNewSheet(context),
             ),
           );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    final isLandscape = mediaQuery.orientation == Orientation.landscape;
+    final txListWidget = Container(
+      height: mediaQuery.size.height * 0.5,
+      child: TransactionList(_userTransactions, _deleteTransaction),
+    );
+    final appBody = SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          if (isLandscape)
+            ..._buildLandscapeContent(
+              mediaQuery,
+              txListWidget,
+            ),
+          if (!isLandscape)
+            ..._buildPortraitContent(
+              mediaQuery,
+              txListWidget,
+            ),
+        ],
+      ),
+    );
+
+    return _buildPage(appBody);
   }
 }
